a1bd6f84287553a847d611a3ec1849ff302df9ab
AnimeshKC <animesh@ualberta.ca>
2020-06-25 08:04:40 -0600

    form setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import "./App.css"
+import useForm from "./components/useForm"
 function App() {
+  const [roverFormValues, setRoverFormValues] = useForm({
+    solNum: 0,
+    cameraType: "any",
+  })
+
+  //const [camera, setCamera] = useForm("All Cameras")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appContainer">
+      <h1 className="appTitle">Mars Rover Photo Display</h1>
+      <h2 className="roverTitle">Curiosity Rover</h2>
+      <div className="appFormContainer">
+        <span>Sol</span>
+        <form>
+          <input
+            type="number"
+            name="solNum"
+            value={roverFormValues.solNum}
+            onChange={setRoverFormValues}
+          ></input>
+        </form>
+        <span>Camera</span>
+        <form>
+          <select
+            name="cameraType"
+            value={setRoverFormValues.cameraType}
+            onChange={setRoverFormValues}
+          >
+            <option value="any"> Any Camera</option>
+            <option value="fhaz">Front Hazard Avoidance Camera</option>
+            <option value="rhaz"> Rear Hazard Avoidance Camera</option>
+            <option value="mast">Mast Camera</option>
+            <option value="chemcam">Chemistry and Camera Complex</option>
+            <option value="mahli">Mars Hand Lens Imager</option>
+            <option value="mardi">Mars Descent Imager</option>
+            <option value="navcam">Navigation Camera</option>
+          </select>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/useForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const useForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues)
+  console.log(values)
+  return [
+    values,
+    (e) => {
+      return setValues({
+        ...values,
+        [e.target.name]: e.target.value,
+      })
+    },
+  ]
+}
+
+export default useForm
