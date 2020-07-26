9ca751a19e1d5e38e546942120de5c01a6803993
AnimeshKC <animesh@ualberta.ca>
2020-07-26 07:57:41 -0600

    code commenting

--- server/server.js
+++ server/server.js
@@ -4,40 +4,65 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 const fetch = require("node-fetch")
+const cors = require("cors")
+
+//remove the below eventually
 app.listen(port, () => console.log(`Server Started on port ${port}`))
 
 app.use(express.json())
+app.use(cors()) //this API shouldn't be restricted
 
-function getUrl(sol, cameraType = "any", page = 1) {
+const TIMEOUT_MS = 10000
+app.use((req, res, next) => {
+  res.setTimeout(TIMEOUT_MS, () => {
+    res.status(408).send(`Timeout exceeded ${(TIMEOUT_MS / 1000).toFixed(1)}s`)
+  })
+  next()
+})
+
+/*
+if a page of 0 is given, getPhotoUrl will show all results, 
+but generally it is preferred to choose a page to reduce load time
+*/
+function getPhotoUrl(sol, cameraType = "any", page = 1) {
   const cameraString = cameraType === "any" ? "" : `&camera=${cameraType}`
   const apiKeyString = `&api_key=${process.env.NASA_API_KEY}`
   const pageString = page > 0 ? `&page=${page}` : ""
   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}${pageString}${cameraString}${apiKeyString}`
   return url
 }
-async function servePhotos(req, res) {
-  try {
-    const reqSol = req.query.sol
-    const reqCamera = req.query.camera || "any"
-    const reqPage = req.query.page || 1
-    const apiUrl = getUrl(reqSol, reqCamera, reqPage)
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((json) => {
-        res.send(json)
-        /*
-        const fullArray = json.photos
-        const photosArray = fullArray.map((element) => element.img_src)
-        res.send(JSON.stringify(photosArray)) */
+
+function servePhotos(req, res) {
+  const reqSol = req.query.sol
+  const reqCamera = req.query.camera || "any"
+  const reqPage = req.query.page || 1
+  const apiUrl = getPhotoUrl(reqSol, reqCamera, reqPage)
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((json) => {
+      //res.send(json)
+      const photosArray = json.photos.map((element) => {
+        return { id: element.id, img_src: element.img_src }
       })
-  } catch (e) {
-    res.send(`Error: ${e.message}`)
-  }
+      res.send(JSON.stringify(photosArray))
+    })
+    .catch((error) => res.send(error))
 }
+
+function getManifest(req, res) {
+  const apiURL = `https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity/?api_key=${process.env.NASA_API_KEY}`
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((json) => res.send(json))
+}
+
 app.get("/api/photos", servePhotos)
+app.get("/api/manifest", getManifest)
+
+//will not need eventually
 app.get("/", (req, res) => {
   const cameraString = `&camera=fhaz`
   const apiKeyString = `&api_key=${process.env.NASA_API_KEY}`
   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&page=1${cameraString}${apiKeyString}`
-  res.send(`${url}`)
+  res.send(`${process.env.NASA_API_KEY}`)
 })
